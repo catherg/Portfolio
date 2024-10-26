bb0d60936df1529fa337fdf919deee39b54036e9
Catherine Guo <Catherineguo2025@u.northwestern.edu>
2024-10-26 20:58:34 +0200

    artifical page added

---
+++ _site/artificial_life.js
@@ -0,0 +1,21 @@
+// script.js
+function toggleSection(elementId) {
+    var content = document.getElementById(elementId);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+function playVideos() {
+    document.getElementById("video1").play();
+    document.getElementById("video2").play();
+    document.getElementById("video3").play();
+}
+
+function pauseVideos() {
+    document.getElementById("video1").pause();
+    document.getElementById("video2").pause();
+    document.getElementById("video3").pause();
+}
